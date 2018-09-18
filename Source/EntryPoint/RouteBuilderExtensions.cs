d445cb69fc62164c47766a35fd5b24317542be30
Einar Ingebrigtsen <einar@dolittle.com>
2018-09-18 20:35:15 +0200

    Adding a small little abstraction for route handlers

---
+++ Source/EntryPoint/RouteBuilderExtensions.cs
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace EntryPoint
+{
+    /// <summary>
+    /// Extensions for <see cref="RouteBuilder"/>
+    /// </summary>
+    public static class route_builder_extensions
+    {
+        /// <summary>
+        /// Map a Post method to a <see cref="ICanHandleRoute"/>
+        /// </summary>
+        /// <typeparam name="T">Type of <see cref="ICanHandleRoute"/> to map to</typeparam>
+        /// <param name="routeBuilder"><see cref="RouteBuilder"/> to extend</param>
+        /// <param name="application"><see cref="IApplicationBuilder"/> application it is for</param>
+        /// <param name="template">Path template</param>
+        public static void MapPost<T>(this RouteBuilder routeBuilder, IApplicationBuilder application, string template) where T:ICanHandleRoute
+        {
+            routeBuilder.MapPost(template, async (request, response, routeData) => {
+                //var handler = (T)application.ApplicationServices.GetService(typeof(T));
+                var handler = Program.Container.Get<T>();
+                await handler.Handle(request, response, routeData);
+            });
+        }
+    }
+}
