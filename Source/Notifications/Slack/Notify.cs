6424d37de5818ae087c20a718665fb43d3688439
Einar Ingebrigtsen <einar@dolittle.com>
2018-09-21 20:40:51 +0200

    Fixing comment

--- Source/Notifications/Slack/Notify.cs
+++ Source/Notifications/Slack/Notify.cs
@@ -8,7 +8,7 @@ namespace Notifications.Slack
     /// https://api.slack.com/docs/message-attachments
     /// https://www.c-sharpcorner.com/UploadFile/bfb43a/integration-with-slack-using-C-Sharp/
     /// https://piotrgankiewicz.com/2016/06/10/effortless-csharp-integration-with-slack/
-    /// </remarks
+    /// </remarks>
     public class Notify
     {
         
