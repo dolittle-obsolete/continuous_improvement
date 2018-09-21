0b67cacad75e1d7787fc7dbf1ac0c865b188539f
Einar Ingebrigtsen <einar@dolittle.com>
2018-09-21 20:40:45 +0200

    Started thinking about notifications

---
+++ Source/Notifications/Slack/Notify.cs
@@ -0,0 +1,16 @@
+namespace Notifications.Slack
+{
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// https://api.slack.com/docs/message-attachments
+    /// https://www.c-sharpcorner.com/UploadFile/bfb43a/integration-with-slack-using-C-Sharp/
+    /// https://piotrgankiewicz.com/2016/06/10/effortless-csharp-integration-with-slack/
+    /// </remarks
+    public class Notify
+    {
+        
+    }
+}
