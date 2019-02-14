a9a3923e482a8d7f4d70256aaba72b95cb07f417
Michael Smith <michael@dolittle.com>
2019-02-14 14:36:00 +0100

    Barebones implementation of Domain for Initiating, Completing and Failing an improvement

---
+++ Source/Web/Features/Improvements/InitiateImprovement.js
@@ -0,0 +1,18 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  This file is an automatically generated Command Proxy
+ *  
+ *--------------------------------------------------------------------------------------------*/
+import { Command } from  '@dolittle/commands';
+
+export class InitiateImprovement extends Command
+{
+    constructor() {
+        super();
+        this.type = 'abbdb153-6500-4e93-9396-d43d0f767f0a';
+
+        this.improvement = '00000000-0000-0000-0000-000000000000';
+        this.forImprovable = '00000000-0000-0000-0000-000000000000';
+        this.version = '';
+        this.isFromPullRequest = false;
+    }
+}
