24db014fb548337462c8920da75451e8237b7087
Michael Smith <michael@dolittle.com>
2019-02-27 11:45:08 +0100

    Specs for completing an improvement

---
+++ Source/Domain.Specs/for_Improvement/when_completing_an_improvement/and_the_improvement_has_already_been_failed.cs
@@ -0,0 +1,23 @@
+﻿
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ * --------------------------------------------------------------------------------------------*/
+
+using System;
+using Dolittle.Machine.Specifications.Events;
+using Events.Improvements;
+using Machine.Specifications;
+using Domain.Improvements;
+
+namespace Domain.Specs.for_Improvement.when_completing_an_improvement
+{
+    public class and_the_improvement_has_already_been_failed : given.a_failed_improvement
+    {
+        static Exception exception;
+        Because of = () => exception = Catch.Exception(() => improvement.Complete());
+
+        It should_not_fail_the_improvement = () => improvement.ShouldNotHaveEvent<ImprovementFailed>();
+        It should_indicate_that_the_improvement_has_already_failed = () => exception.ShouldBeOfExactType<ImprovementAlreadyFailed>();
+    }
+}
