56cad024f17376d1054334c1b2a2b08cf4c6c816
Michael Smith <michael@dolittle.com>
2019-03-05 11:12:06 +0100

    Add StepId to encapsulate the name / step no pattern / regex

---
+++ Source/Concepts/Improvements/StepId.cs
@@ -0,0 +1,84 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ * --------------------------------------------------------------------------------------------*/
+
+using System.Text.RegularExpressions;
+using Dolittle.Concepts;
+
+namespace Concepts.Improvements
+{
+    /// <summary>
+    /// Represents a semantic <see cref="StepId" />
+    /// </summary>
+    public class StepId : Value<StepId>
+    {
+        static readonly Regex _regex = new Regex(@"^step-(\d+)-(\d+)-?(.*)$", RegexOptions.Compiled);
+
+        /// <summary>
+        ///The state of a <see cref="StepId" /> that has not been set.
+        /// </summary>
+        public static StepId Empty { get; } = string.Empty;
+
+        /// <summary>
+        /// Instantiate a <see cref="StepId" /> with the Empty state
+        /// </summary>
+        /// <returns></returns>
+        public StepId() : this(string.Empty)
+        {}
+
+        /// <summary>
+        /// Instantiate a <see cref="StepId" /> from the string representation
+        /// </summary>
+        /// <param name="value"></param>
+        public StepId(string value)
+        {
+            var match = _regex.Match(value);
+            if(match.Success)
+            {
+                StepNumber = int.Parse(match.Groups[1].Value);
+                SubStepNumber = int.Parse(match.Groups[2].Value);
+                Container = match.Groups[3].Value;
+            }
+            else
+            {
+                StepNumber = StepNumber.Empty;
+                SubStepNumber = StepNumber.Empty;
+                Container = string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Instantiate a <see cref="StepId" /> from the component parts
+        /// </summary>
+        /// <param name="stepNumber">The number of the step</param>
+        /// <param name="subStepNumber">The number of the substep</param>
+        /// <param name="container">The name of the container</param>
+        public StepId(int stepNumber, int subStepNumber, string container)
+        {
+            StepNumber = stepNumber;
+            SubStepNumber = subStepNumber;
+            Container = container;
+        }        
+
+        /// <summary>
+        /// Implicitly convert a string to a <see cref="StepId" />
+        /// </summary>
+        /// <param name="value"></param>
+        public static implicit operator StepId(string value) => new StepId(value);
+
+        public StepNumber StepNumber { get; }
+        public StepNumber SubStepNumber { get; }
+        public string Container { get; }
+
+        public override string ToString()
+        {
+            return $"step-{StepNumber}-{SubStepNumber}-{Container??string.Empty}";
+        }
+
+        public bool IsValid()
+        {
+            return StepNumber != StepNumber.Empty;
+        }
+    }
+}
